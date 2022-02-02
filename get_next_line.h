38cfb59ba16441a44a1d85b92f9ce910ddb19d99
bperraud <74benji@live.fr>
2022-02-02 17:26:59 +0100

    header

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:23:43 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/01 23:54:40 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:18:22 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 5
+//# define BUFFER_SIZE 
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -21,6 +21,9 @@
 char	*get_next_line(int fd);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	**ft_split(char const *s);
+char    *ft_strncpy(char *dest, const char *src, unsigned int n);
+char	**ft_split(char const *s, const char c);
+size_t  ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 
 #endif

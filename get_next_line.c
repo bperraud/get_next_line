b4d57e4133bbf5019404600ba2cd8c281fb40dd3
bperraud <74benji@live.fr>
2022-02-02 16:53:12 +0100

    save --

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:22:45 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/02 16:21:42 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/02 16:52:49 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,22 @@ int main()
 char	*get_next_line(int fd)
 {	
 	int				ret;
-	char			buff[BUFFER_SIZE];
+	static char		buff[BUFFER_SIZE];
 	char			*line;
+	//char			*temp;
 	char			**split;
-	static char 	save[BUFFER_SIZE];
 
-	ft_strncpy(buff, save, BUFFER_SIZE);		// il ne peut pas avoir + de BUFFERSIZE element qui reste 
 	ret = BUFFER_SIZE;
-	line = "";	// pour le premier join 
+	//line = "";
+	//line = malloc(2 * sizeof(char));
+	line = "";
+	//line[1] = '\0' ;
 	while (ret == BUFFER_SIZE && ft_memchr(buff, '\n', BUFFER_SIZE) == NULL) // on lit au moins 1 octet
 	{
 		// free avant chaque join 
+		//temp = line;
 		line = ft_strjoin(line, buff);
+		//free(temp);
 		ret = read(fd, buff, BUFFER_SIZE);		// continue a lire
 	}
 	if (ret == -1)	// erreur de read, free ?
@@ -52,8 +56,10 @@ char	*get_next_line(int fd)
 		split = ft_split(buff, '\n');		// enlever de line + stocker la fin du read apres le \n
 		line = ft_strjoin(line, split[0]);
 		//printf("line : %s\n", line);
-		ft_strncpy(save, split[1], BUFFER_SIZE);
-		//printf("save : %s\n", save);
+		ft_strncpy(buff, split[1], BUFFER_SIZE);
+		//free(split[0]);
+		//free(split[1]);
+		free(split);
 	}
 	else // fin du fichier = pas de '\n'	/ vraiment utile ? avant d'avoir ret == 0 le fichier a deja été lu en entier 
 	{

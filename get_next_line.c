8baffe0266f76c3a6f05c4cf93dbca2557b778c3
bperraud <74benji@live.fr>
2022-02-02 17:16:25 +0100

    free split

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:22:45 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/02 17:14:49 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:16:12 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ char	*get_next_line(int fd)
 	line = ft_strdup("");
 	while (ret == BUFFER_SIZE && ft_memchr(buff, '\n', BUFFER_SIZE) == NULL) // on lit au moins 1 octet
 	{
-		// free avant chaque join 
 		temp = line;
 		line = ft_strjoin(line, buff);
 		free(temp);
@@ -49,13 +48,11 @@ char	*get_next_line(int fd)
 		return (NULL);
 	else if (ret == BUFFER_SIZE)	// fin de ligne  
 	{
-		//printf("buff : %s\n", buff);
 		split = ft_split(buff, '\n');		// enlever de line + stocker la fin du read apres le \n
 		line = ft_strjoin(line, split[0]);
-		//printf("line : %s\n", line);
 		ft_strncpy(buff, split[1], BUFFER_SIZE);
-		//free(split[0]);
-		//free(split[1]);
+		free(split[0]);
+		free(split[1]);
 		free(split);
 	}
 	else // fin du fichier = pas de '\n'	/ vraiment utile ? avant d'avoir ret == 0 le fichier a deja été lu en entier 

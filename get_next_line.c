3c645e20fbebd832f14c4119daf5fad9a2d2d505
bperraud <74benji@live.fr>
2022-02-02 01:27:24 +0100

    works with leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:22:45 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/02 00:45:55 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/02 01:26:58 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,27 @@ char	*get_next_line(int fd)
 	static char 	save[BUFFER_SIZE];
 
 	ft_strncpy(buff, save, BUFFER_SIZE);		// il ne peut pas avoir + de BUFFERSIZE element qui reste 
-	ret = 1;
+	ret = BUFFER_SIZE;
 	line = "";	// pour le premier join 
-	while (ret > 0 && ft_memchr(buff, '\n', BUFFER_SIZE) == NULL) // on lit au moins 1 octet
+	while (ret == BUFFER_SIZE && ft_memchr(buff, '\n', BUFFER_SIZE) == NULL) // on lit au moins 1 octet
 	{
 		// free avant chaque join 
-		
 		line = ft_strjoin(line, buff);
 		ret = read(fd, buff, BUFFER_SIZE);		// continue a lire
-		
 	}
 	if (ret == -1)	// erreur de read, free ?
 		return (NULL);
-	else if (ret == 0) // fin du fichier = pas de '\n'	
+	else if (ret == BUFFER_SIZE)	// fin de ligne  
 	{
-
-	}
-	else	// fin de ligne  
-	{
-		split = ft_split(buff);		// enlever de line + stocker la fin du read apres le \n
+		split = ft_split(buff, '\n');		// enlever de line + stocker la fin du read apres le \n
 		line = ft_strjoin(line, split[0]);
 		ft_strncpy(save, split[1], BUFFER_SIZE);
+	}
+	else // fin du fichier = pas de '\n'	/ vraiment utile ? avant d'avoir ret == 0 le fichier a deja été lu en entier 
+	{
+		ft_strncpy(buff, buff, ret);
+		line = ft_strjoin(line, buff);
+		return (line);
 	}
 	return (line);
 }

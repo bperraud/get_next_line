83c21b14cfcde6766b7080bf1154a24c206beab2
bperraud <74benji@live.fr>
2022-02-02 17:29:42 +0100

    main

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:23:43 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/01 23:54:40 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:25:59 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 5
+//# define BUFFER_SIZE 5
 
 # include <stdlib.h>
 # include <unistd.h>

f25707050c8413e21eae99a4776ce58c0990f0e0
bperraud <74benji@live.fr>
2022-02-06 19:14:47 +0100

    up

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -12,90 +12,92 @@
 
 #include "get_next_line_bonus.h"
 
-char	*get_next_line(int fd)
+size_t	ft_strlen(const char *s)
 {
-	if (fd < 0 || BUFFER_SIZE == 0 || BUFFER_SIZE > 10000000000 || fd > FOPEN_MAX)
-        return (NULL);
-	else 
-		return (true_gnl(fd));	
-}
+	int	i;
 
-char	*true_gnl(int fd)
-{	
-	long			ret;
-	static char		buff[FOPEN_MAX][BUFFER_SIZE + 1];
-	char			*line;
-	char			*temp;
-	long			i;
-
-	ret = BUFFER_SIZE;
-	line = ft_strdup("");
-	while (ret == BUFFER_SIZE && ft_memchr(buff[fd], '\n', BUFFER_SIZE) == NULL)
-	{
-		temp = line;
-		line = ft_strjoin(line, buff[fd]);
-		free(temp);
-		ret = read(fd, buff[fd], BUFFER_SIZE);	
-	}
-	temp = line;
-	i = BUFFER_SIZE + 1;
-	while (i > ret - 1)
-		buff[fd][i--] = 0;
-	return (true_gnl_2(ret, line, buff[fd], temp));
-}
-
-char	*true_gnl_2(long ret, char *line, char *buff, char *temp)
-{
-	if (ret == -1 || ret == 0)	
-	{
-		if (ret == 0 && line[0] != '\0')
-			return (line);
-		free(temp);
-		return (NULL);
-	}
-	else
-	{			
-		if (ft_memchr(buff, '\n', BUFFER_SIZE) != NULL)		
-			line = end_of_line(line, buff);
-		else	
-		{
-			line = ft_strjoin(line, buff);
-			while (ret--)
-				*buff++ = 0;
-		}
-	}			
-	free(temp);
-	return (line);
-}
-
-char	*end_of_line(char *line, char *buff)
-{
-	char	**split;
-
-	split = ft_split(buff, '\n');		
-	line = ft_strjoin(line, split[0]);
-	ft_strncpy(buff, split[1], BUFFER_SIZE);
-	free(split[0]);
-	free(split[1]);
-	free(split);
-	return (line);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	char	*dest;
-	int		i;
-
-	dest = malloc(((ft_strlen(s1)) + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		dest[i] = s1[i];
+	while (s[i])
 		i++;
+	return (i);
+}
+
+void	*ft_memchr(const void *s, int c, long n)
+{
+	const unsigned char	*cs;
+	unsigned char		uc;
+
+	cs = s;
+	uc = c;
+	while (n && *cs != uc)
+	{
+		n--;
+		cs++;
 	}
-	dest[i] = '\0';
+	if (n)
+		return ((void *) cs);
+	else
+		return (NULL);
+}
+
+char	*ft_strncpy(char *dest, const char *src, long n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (src)
+	{
+		while (src[i] != '\0' && i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		while (i <= n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
 	return (dest);
 }
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	lens1;
+	size_t	lens2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	str = malloc((lens1 + lens2 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strncpy(str, s1, lens1);
+	ft_strncpy(str + lens1, s2, ft_strlen(s2));
+	return (str);
+}
+
+char	**ft_split(char const *str, const char c)
+{
+	char	**dest;
+	int		i;
+
+	i = 0;
+	dest = malloc(2 * sizeof(char *));
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			i++; 
+			dest[0] = malloc(i * sizeof(char));
+			dest[1] = malloc((BUFFER_SIZE - i) * sizeof(char));
+			ft_strncpy(dest[0], str, i);
+			ft_strncpy(dest[1], str + i, BUFFER_SIZE - i);
+			break;
+		}
+		i++;
+	}
+	return (dest);
+}

2aaa8220e2b6ed4bd05eb12b5f571b783eaa57ad
bperraud <74benji@live.fr>
2022-02-06 18:48:00 +0100

    ftbzero

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:22:45 by bperraud          #+#    #+#             */
-/*   Updated: 2022/02/06 18:44:12 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/02/06 18:47:57 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,9 @@ char	*get_next_line(int fd)
 	}
 	temp = line;
 	//ft_bzero(buff + ret, BUFFER_SIZE + 1);
-	i = 0;
-	while (ret--)
-		buff[i++] = 0;
+	i = BUFFER_SIZE + 1;
+	while (i > ret)
+		buff[i--] = 0;
 	return (get_next_line_2(ret, line, buff, temp));
 }
 
